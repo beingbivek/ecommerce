a8372b8f344d9bac2ae1357b2fcecd22514fadcd
Bivek Thapa <beingbivek@gmail.com>
2022-02-23 00:06:29 +0545

    Added Post method for login

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,6 +1,6 @@
 export 'appbar.dart';
 
-const loginAPI = '';
+const ecommetceApi = 'https://ecommerceapi.rgbitsolution.com.np/api/';
 
 const apiKey = '173116833e0f3f58005a87baafe41e8a';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
--- lib/views/loginpage.dart
+++ lib/views/loginpage.dart
@@ -1,8 +1,48 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+
+class Post {
+  final String? userId;
+  final String? pass;
+
+  Post({this.userId, this.pass});
+
+  factory Post.fromJson(Map json) {
+    return Post(
+      userId: json['userId'],
+      pass: json['pass'],
+    );
+  }
+
+  Map toMap() {
+    var map = new Map();
+    map["userId"] = userId;
+    map["pass"] = pass;
+
+    return map;
+  }
+}
+
+Future createPost(String url, {Map? body}) async {
+  return http.post(Uri(), body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400) {
+      throw new Exception("Error while fetching data");
+    }
+    return Post.fromJson(json.decode(response.body));
+  });
+}
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  final Future? post;
+  const LoginPage({Key? key, this.post}) : super(key: key);
+  static final createposturl =
+      'https://ecommerceapi.rgbitsolution.com.np/api/login';
+  static TextEditingController userController = TextEditingController();
+  static TextEditingController passController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,15 +78,17 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   children: [
                     TextFormField(
+                      controller: userController,
                       decoration: InputDecoration(labelText: 'Username'),
                       validator: (value) {
-                        user = value;
+                        // user = value;
                       },
                       onChanged: (value) {
                         user = value;
                       },
                     ),
                     TextFormField(
+                      controller: passController,
                       decoration: InputDecoration(labelText: 'Password'),
                       obscureText: true,
                       onChanged: (value) {
@@ -57,10 +99,19 @@ class LoginPage extends StatelessWidget {
                       height: 50,
                     ),
                     MaterialButton(
-                      onPressed: () {
+                      onPressed: () async {
                         print(user);
-                        Navigator.of(context)
-                            .pushNamed('/home', arguments: user);
+                        try {
+                          Post newPost = new Post(userId: user, pass: pass);
+                          Post p = await createPost(createposturl,
+                              body: newPost.toMap());
+                          print(p.userId);
+                        } catch (e) {
+                          print(e);
+                        }
+
+                        // Navigator.of(context)
+                        //     .pushNamed('/home', arguments: user);
                       },
                       color: Colors.amber,
                       child: Text('Login'),
